0e94527b6199e493c5ec20af0dff146a1b776b9b
enespekdas <enes.pekdas@gmail.com>
2022-02-09 20:37:16 +0300

    composite action developing v1.7.1

--- build.py
+++ build.py
@@ -20,7 +20,7 @@ osSystem = platform.system()
 if parseJson.details[0]["dockerFilePath"] == "":
     parseJson.details[0]["dockerFilePath"] = "./Dockerfile"
 buildYap=subprocess.check_output("podman build --tag "+parseJson.details[0]["imageName"]+":"+parseJson.details[0]["tag"]+" -f "+parseJson.details[0]["dockerFilePath"], shell=True)
-print(buildYap)
+
 lsYap=subprocess.check_output("echo podman imajlari listeleniyor", shell=True)
 print(lsYap)
 podmanImages=subprocess.check_output("podman images", shell=True)
